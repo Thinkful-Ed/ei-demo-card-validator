6efe3569047b280930465e3f27148c7e69fa1cf2
Joe Turner <joe@oampo.co.uk>
2018-03-01 12:59:36 +0000

    Starting point

---
+++ src/is-valid.js
@@ -0,0 +1,16 @@
+export default function isValid(number) {
+    const check = number
+        .toString()
+        .split('')
+        .map(digit => parseInt(digit, 10))
+        .reverse()
+        .map((digit, index) => {
+            if (index % 2 === 0) {
+                return digit;
+            }
+            const doubled = digit * 2;
+            return doubled <= 9 ? doubled : doubled - 9;
+        })
+        .reduce((sum, value) => sum + value, 0);
+    return check % 10 === 0;
+}
