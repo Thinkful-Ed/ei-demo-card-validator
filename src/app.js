f3d4d2dca3f6e240333f24cbebeddb50d4e9150e
Joe Turner <joe@oampo.co.uk>
2018-03-01 13:08:31 +0000

    Add state

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Input from './input';
 import Validity from './validity';
 
-export default function App() {
-    return (
-        <div className="card-validity">
-            <Input />
-            <Validity number={79927398713} />
-        </div>
-    );
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 79927398713
+        };
+    }
+
+    render() {
+        return (
+            <div className="card-validity">
+                <Input />
+                <Validity number={this.state.number} />
+            </div>
+        );
+    }
 }
